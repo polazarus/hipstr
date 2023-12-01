ccaf19345698fab23c09d31c0f6bf982f8981963
polazarus <polazarus@users.noreply.github.com>
2023-12-01 14:03:31 +0100

    chore: add integration test for borrow

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::hint::black_box;
 
 use hipstr::HipStr;
@@ -9,4 +10,14 @@ fn test_eq() {
     assert_eq!(h, h2);
     let h3 = h.strip_prefix("a").unwrap();
     assert_eq!(h3, "bc");
+}
+
+#[test]
+fn test_borrow() {
+    let s = String::from("abc");
+    let c = Cow::Borrowed(&s[..]);
+    let h = HipStr::from(c);
+    assert!(h.is_borrowed());
+    let h_arr = [h, HipStr::borrowed("abc")];
+    assert!(h_arr[1].is_borrowed());
 }
