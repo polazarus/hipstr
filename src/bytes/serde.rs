f18381969390a79e04b5738f70190319486d24ee
polazarus <polazarus@users.noreply.github.com>
2024-12-28 16:42:40 +0100

    perf: less alloc in deserialization of HipByt and HipPath
    - cleanup serde for HipStr
    - make deserialization to hipstr more tolerant
      (`String::deserialize` supports byte inputs)
    - add more tests

--- src/bytes/serde.rs
+++ src/bytes/serde.rs
@@ -1,9 +1,47 @@
+//! `serde` support for `HipByt`.
+//!
+//! This module provides support for serializing and deserializing `HipByt`
+//! using [`serde`]. It is enabled by default when the `serde` feature is
+//! enabled.
+//!
+//! # Examples
+//!
+//! ```
+//! use hipstr::HipByt;
+//!
+//! let s = HipByt::from(b"hello");
+//! let serialized = serde_json::to_string(&s).unwrap();
+//! assert_eq!(serialized, "[104,101,108,108,111]");
+//!
+//! let deserialized: HipByt = serde_json::from_str(r#""hello""#).unwrap();
+//! assert_eq!(deserialized, s);
+//! ```
+//!
+//! # Notable aspects of the implementations
+//!
+//! Unlike the `Vec<T>` generic implementation which treats data as a generic
+//! sequence, `HipByt` uses the more efficient byte sequence specific API for
+//! serialization (similar to the [`serde_bytes`] crate). Note that the actual
+//! outcome of a serialization depends on the underlying format's support for
+//! byte sequences.
+//!
+//! During deserialization, this implementation minimizes allocations by reusing
+//! the deserializer's internal buffer if possible.
+//!
+//! [`serde_bytes`]: https://docs.rs/serde_bytes
+
+use core::marker::PhantomData;
+
+use serde::de::Visitor;
 use serde::{Deserialize, Serialize};
 
 use super::HipByt;
-use crate::alloc::borrow::Cow;
+use crate::alloc::fmt;
+use crate::alloc::string::String;
 use crate::alloc::vec::Vec;
 use crate::Backend;
+
+const EXPECTING: &str = "a byte array";
 
 impl<B> Serialize for HipByt<'_, B>
 where
@@ -17,6 +55,59 @@ where
     }
 }
 
+/// Deserializer's visitor for owned `HipByt`.
+struct OwnedVisitor<'borrow, B: Backend>(PhantomData<HipByt<'borrow, B>>);
+
+impl<'de, 'borrow, B: Backend> Visitor<'de> for OwnedVisitor<'borrow, B> {
+    type Value = HipByt<'borrow, B>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(EXPECTING)
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v))
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v))
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v.as_bytes()))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v.into_bytes()))
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let len = core::cmp::min(seq.size_hint().unwrap_or(0), 4096);
+        let mut bytes = Vec::with_capacity(len);
+
+        while let Some(b) = seq.next_element()? {
+            bytes.push(b);
+        }
+
+        Ok(HipByt::from(bytes))
+    }
+}
+
 impl<'de, B> Deserialize<'de> for HipByt<'_, B>
 where
     B: Backend,
@@ -25,8 +116,74 @@ where
     where
         D: serde::Deserializer<'de>,
     {
-        let v: Vec<u8> = serde_bytes::deserialize(deserializer)?;
-        Ok(Self::from(v))
+        deserializer.deserialize_bytes(OwnedVisitor(PhantomData))
+    }
+}
+
+/// Deserializer's visitor for borrowed `HipByt`.
+struct BorrowedVisitor<'de, B: Backend>(PhantomData<HipByt<'de, B>>);
+
+impl<'de, B: Backend> Visitor<'de> for BorrowedVisitor<'de, B> {
+    type Value = HipByt<'de, B>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(EXPECTING)
+    }
+
+    fn visit_borrowed_bytes<E>(self, v: &'de [u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::borrowed(v))
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v))
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v))
+    }
+
+    fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::borrowed(v.as_bytes()))
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v.as_bytes()))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(HipByt::from(v.into_bytes()))
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let len = core::cmp::min(seq.size_hint().unwrap_or(0), 4096);
+        let mut bytes = Vec::with_capacity(len);
+
+        while let Some(b) = seq.next_element()? {
+            bytes.push(b);
+        }
+
+        Ok(HipByt::from(bytes))
     }
 }
 
@@ -62,8 +219,7 @@ where
     D: serde::Deserializer<'de>,
     B: Backend,
 {
-    let cow: Cow<'de, [u8]> = serde_bytes::Deserialize::deserialize(deserializer)?;
-    Ok(HipByt::from(cow))
+    deserializer.deserialize_bytes(BorrowedVisitor(PhantomData))
 }
 
 #[cfg(test)]
--- src/string/serde.rs
+++ src/string/serde.rs
@@ -1,13 +1,45 @@
+//! `serde` support for `HipStr`.
+//!
+//! This module provides support for serializing and deserializing `HipStr`
+//! using [`serde`]. It is enabled by default when the `serde` feature is
+//! enabled.
+//!
+//! # Examples
+//!
+//! ```
+//! use hipstr::HipStr;
+//!
+//! let s = HipStr::from("hello");
+//! let serialized = serde_json::to_string(&s).unwrap();
+//! assert_eq!(serialized, r#""hello""#);
+//!
+//! let deserialized: HipStr = serde_json::from_str(&serialized).unwrap();
+//! assert_eq!(deserialized, s);
+//! ```
+//!
+//! # Notable aspects of the implementation
+//!
+//! During deserialization, this implementation minimizes allocations by
+//! reusing the deserializer's internal buffer if possible.
+//!
+//! Like `String`'s serde implementation, this implementation supports
+//! UTF-8 encoded byte sequence as input.
+
+#![allow(clippy::option_if_let_else)]
+
+use core::fmt;
 use core::marker::PhantomData;
 
-use serde::de::{Error, Visitor};
+use serde::de::{Error, Unexpected, Visitor};
 use serde::{de, Deserialize, Deserializer, Serialize};
 
 use super::HipStr;
-use crate::alloc::borrow::Cow;
-use crate::alloc::fmt;
-use crate::alloc::string::{String, ToString};
+use crate::alloc::string::String;
+use crate::alloc::vec::Vec;
+use crate::bytes::HipByt;
 use crate::Backend;
+
+const EXPECTING: &str = "a string";
 
 impl<B> Serialize for HipStr<'_, B>
 where
@@ -22,15 +54,14 @@ where
     }
 }
 
-struct HipStrVisitor<'b, B> {
-    data: PhantomData<&'b B>,
-}
+/// Deserializer's visitor for owned `HipStr`.
+struct OwnedVisitor<'borrow, B: Backend>(PhantomData<HipStr<'borrow, B>>);
 
-impl<'a, 'b, B: Backend> Visitor<'a> for HipStrVisitor<'b, B> {
+impl<'b, B: Backend> Visitor<'_> for OwnedVisitor<'b, B> {
     type Value = HipStr<'b, B>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("a string")
+        formatter.write_str(EXPECTING)
     }
 
     #[inline]
@@ -47,6 +78,29 @@ impl<'a, 'b, B: Backend> Visitor<'a> for HipStrVisitor<'b, B> {
         E: Error,
     {
         Ok(HipStr::from(v))
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match core::str::from_utf8(v) {
+            Ok(s) => Ok(HipStr::from(s)),
+            Err(_) => Err(Error::invalid_value(Unexpected::Bytes(v), &self)),
+        }
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match String::from_utf8(v) {
+            Ok(s) => Ok(HipStr::from(s)),
+            Err(e) => Err(Error::invalid_value(
+                Unexpected::Bytes(&e.into_bytes()),
+                &self,
+            )),
+        }
     }
 }
 
@@ -59,41 +113,72 @@ where
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_str(HipStrVisitor::<B> {
-            data: PhantomData::default(),
-        })
+        deserializer.deserialize_str(OwnedVisitor(PhantomData))
     }
 }
 
-/// Minimal string cow visitor
-struct CowVisitor;
+/// Deserializer's visitor for borrowed `HipStr`.
+struct BorrowedVisitor<'de, B: Backend>(PhantomData<HipByt<'de, B>>);
 
-impl<'de> de::Visitor<'de> for CowVisitor {
-    type Value = Cow<'de, str>;
+impl<'de, B: Backend> Visitor<'de> for BorrowedVisitor<'de, B> {
+    type Value = HipStr<'de, B>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("a string")
+        formatter.write_str(EXPECTING)
     }
 
     fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
     where
         E: de::Error,
     {
-        Ok(Cow::Borrowed(v))
+        Ok(HipStr::borrowed(v))
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
     where
         E: de::Error,
     {
-        Ok(Cow::Owned(v.to_string()))
+        Ok(HipStr::from(v))
     }
 
     fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
     where
         E: de::Error,
     {
-        Ok(Cow::Owned(v))
+        Ok(HipStr::from(v))
+    }
+
+    fn visit_borrowed_bytes<E>(self, v: &'de [u8]) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match core::str::from_utf8(v) {
+            Ok(s) => Ok(HipStr::borrowed(s)),
+            Err(_) => Err(Error::invalid_value(Unexpected::Bytes(v), &self)),
+        }
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match core::str::from_utf8(v) {
+            Ok(s) => Ok(HipStr::from(s)),
+            Err(_) => Err(Error::invalid_value(Unexpected::Bytes(v), &self)),
+        }
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match String::from_utf8(v) {
+            Ok(s) => Ok(HipStr::from(s)),
+            Err(e) => Err(Error::invalid_value(
+                Unexpected::Bytes(&e.into_bytes()),
+                &self,
+            )),
+        }
     }
 }
 
@@ -128,8 +213,7 @@ where
     D: serde::Deserializer<'de>,
     B: Backend,
 {
-    let cow: Cow<'de, str> = deserializer.deserialize_str(CowVisitor)?;
-    Ok(HipStr::from(cow))
+    deserializer.deserialize_str(BorrowedVisitor(PhantomData))
 }
 
 #[cfg(test)]
