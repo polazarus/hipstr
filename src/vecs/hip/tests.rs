e4935f001b72c035e6f7692c1de69296755bbae6
polazarus <polazarus@users.noreply.github.com>
2025-09-12 17:00:25 +0200

    wip

--- src/vecs/hip/tests.rs
+++ src/vecs/hip/tests.rs
@@ -4,6 +4,19 @@ use crate::vecs::hip::HipVec;
 use crate::Arc;
 
 #[test]
+fn size_of_hipvec() {
+    assert_eq!(
+        size_of::<HipVec<'static, u8, Arc>>(),
+        3 * size_of::<usize>()
+    );
+
+    assert_eq!(
+        size_of::<HipVec<'static, usize, Arc>>(),
+        3 * size_of::<usize>()
+    );
+}
+
+#[test]
 fn new() {
     let h = HipVec::<u8, Arc>::new();
     assert_eq!(h.len(), 0);
