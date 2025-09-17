e85dcc1fab841780ab90f0b020ee5413f2805b3a
polazarus <polazarus@users.noreply.github.com>
2025-09-17 14:51:39 +0200

    wip

--- src/vecs/inline/length.rs
+++ src/vecs/inline/length.rs
@@ -1,7 +1,7 @@
 use core::ops::{Div, Rem, Sub};
 
 use generic_array::ArrayLength;
-use typenum::{NonZero, Quot, Sub1, B1, U, U0, U12, U16, U20, U24, U4, U8};
+use typenum::{NonZero, Quot, Sub1, Unsigned, B1, U, U0, U12, U16, U20, U24, U4, U8};
 
 /// Size of a pointer on the current platform, in bytes.
 #[cfg(target_pointer_width = "16")]
@@ -23,9 +23,14 @@ pub type PointerSize = U8;
 compile_error!("unsupported target pointer width");
 
 /// Trait for type numbers that are divisible by another.
-pub trait Divisible<U>: Div<U> + Rem<U, Output = U0> {}
+pub trait Divisible<U: Unsigned + NonZero>: Unsigned + Div<U> + Rem<U, Output = U0> {}
 
-impl<T, U> Divisible<U> for T where T: Div<U> + Rem<U, Output = U0> {}
+impl<T, U> Divisible<U> for T
+where
+    T: Unsigned + Div<U> + Rem<U, Output = U0>,
+    U: Unsigned + NonZero,
+{
+}
 
 const _DIVISIBLE_ASSERTS: () = {
     const fn is_divisible<T: Divisible<U<4>>>() {}
@@ -40,7 +45,7 @@ const _DIVISIBLE_ASSERTS: () = {
 /// Trait for type numbers that can be used as inline lengths.
 ///
 /// Basically, this means they are non-zero and divisible by the pointer size.
-pub trait InlineLength: NonZero + ArrayLength + Seal {}
+pub trait InlineLength: NonZero + ArrayLength + Divisible<PointerSize> + Seal {}
 
 pub trait Seal {
     type Words: ArrayLength;
