3cc8523ad6400f3574c0fc9c5de7b7711d6132dd
polazarus <polazarus@users.noreply.github.com>
2025-12-05 11:48:04 +0100

    remove cruft

--- src/bytes/raw/borrowed/tests.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-#[test]
-#[allow(clippy::clone_on_copy)]
-fn test_clone() {
-    let a = Borrowed::new(b"abc");
-    let b = a.clone();
-    assert_eq!(a.as_slice(), b.as_slice());
-}
